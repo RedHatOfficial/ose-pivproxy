f597108f9fb52086e808738b470f7e807c69f6ba
Chris Ruffalo <chris.ruffalo@gmail.com>
2017-09-25 16:24:12 -0400

    updated start and proxy configuration

--- start.sh
+++ start.sh
@@ -14,5 +14,9 @@ echo "Running with user ${USER_NAME} (${USER_ID}) and group ${GROUP_ID}"
 
 # handle pre-startup configuration
 
+# create client certificate bundle
+cat /etc/pki/tls/private/tls.crt > /tmp/proxy_client.pem
+cat /etc/pki/tls/private/tls.key >> /tmp/proxy_client.pem
+
 # start apache
 /usr/sbin/httpd -DFOREGROUND
