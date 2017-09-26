e1a72a14becda8a9494b3b0e236e2ac3d352d2f1
Chris Ruffalo <chris.ruffalo@gmail.com>
2017-09-26 10:58:17 -0400

    revert to using /tmp

--- start.sh
+++ start.sh
@@ -16,8 +16,8 @@ echo "Running with user ${USER_NAME} (${USER_ID}) and group ${GROUP_ID}"
 export CURRENT_NAMESPACE=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
 
 # create client certificate bundle
-cat /secrets/tls.crt > /apache/proxy_client.pem
-cat /secrets/tls.key >> /apache/proxy_client.pem
+cat /secrets/tls.crt > /tmp/proxy_client.pem
+cat /secrets/tls.key >> /tmp/proxy_client.pem
 
 # start apache
 /usr/sbin/httpd -DFOREGROUND
